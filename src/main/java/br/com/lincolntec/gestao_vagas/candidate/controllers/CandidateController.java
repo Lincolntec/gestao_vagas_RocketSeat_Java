b0c0a3a071569d1dbe9f79aeb7b513433009e5db
Lincoln <lincoln_tec@hotmail.com>
2023-12-23 13:49:06 -0300

    Conclusion candidate classes.

--- src/main/java/br/com/lincolntec/gestao_vagas/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/lincolntec/gestao_vagas/candidate/controllers/CandidateController.java
@@ -2,32 +2,32 @@ package br.com.lincolntec.gestao_vagas.candidate.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lincolntec.gestao_vagas.candidate.CandidateEntity;
-import br.com.lincolntec.gestao_vagas.candidate.CandidateRepository;
-import br.com.lincolntec.gestao_vagas.exceptions.UserFoundException;
+import br.com.lincolntec.gestao_vagas.candidate.useCase.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
-    @Autowired
-    private CandidateRepository candidateRepository;
+   @Autowired
+   private CreateCandidateUseCase candidateUseCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
 
-        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-        .ifPresent((user) -> {
-            throw new UserFoundException();
-        });
-        return this.candidateRepository.save(candidateEntity);
-
+        try {
+            var result = this.candidateUseCase.execute(candidateEntity);    
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
     
 }
--- src/main/java/br/com/lincolntec/gestao_vagas/candidate/useCase/CreateCandidateUseCase.java
+++ src/main/java/br/com/lincolntec/gestao_vagas/candidate/useCase/CreateCandidateUseCase.java
@@ -1,5 +1,24 @@
 package br.com.lincolntec.gestao_vagas.candidate.useCase;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.lincolntec.gestao_vagas.candidate.CandidateEntity;
+import br.com.lincolntec.gestao_vagas.candidate.CandidateRepository;
+import br.com.lincolntec.gestao_vagas.exceptions.UserFoundException;
+
+@Service
 public class CreateCandidateUseCase {
+
+     @Autowired
+    private CandidateRepository candidateRepository;
     
+    public CandidateEntity execute(CandidateEntity candidateEntity){
+         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+        .ifPresent((user) -> {
+            throw new UserFoundException();
+        });
+        return this.candidateRepository.save(candidateEntity);
+
+    }
 }
